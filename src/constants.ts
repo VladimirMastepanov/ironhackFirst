3a2827c914b8caee8d0e57089249209968fd6def
VladimirMastepanov <rozenkranc4@gmail.com>
2025-04-29 09:42:11 +0200

    add contact.html, projects.html, 404.html to vite.config

--- src/constants.ts
+++ src/constants.ts
@@ -2,22 +2,22 @@ export const API_QUERY = 'https://raw.githubusercontent.com/ironhack-jc/mid-term
 
 export const NUMBER_PROJECRS_FOR_DISPLAY = 3;
 
+// export const PATH_TO_LOGO = '/assets/Circle.svg';
+// export const PATH_TO_HOME = '/index.html';
+// export const PATH_TO_PROJECTS = '/src/app/pages/projects/1.html';
+// export const PATH_TO_SERVISES = '/src/app/pages/servises/1.html';
+// export const PATH_TO_CONTACT = '/src/app/pages/contact/contact.html';
+// export const PATH_TO_TOGGLE = '/assets/sidebar/close.svg';
+// export const PATH_TO_MAIL_ICON = '/assets/newsletter/icon/mail.svg';
+// export const PATH_TO_SIDEBAR_BUTTON_OPEN = '/assets/sidebar/open.svg';
+// export const PATH_TO_SIDEBAR_BUTTON_CLOSE = '/assets/sidebar/close.svg';
+
 export const PATH_TO_LOGO = '/assets/Circle.svg';
 export const PATH_TO_HOME = '/index.html';
-export const PATH_TO_PROJECTS = '/src/app/pages/projects/1.html';
-export const PATH_TO_SERVISES = '/src/app/pages/servises/1.html';
-export const PATH_TO_CONTACT = '/src/app/pages/contact/contact.html';
+export const PATH_TO_PROJECTS = '/projects.html';
+export const PATH_TO_SERVISES = '/servises.html';
+export const PATH_TO_CONTACT = '/contact.html';
 export const PATH_TO_TOGGLE = '/assets/sidebar/close.svg';
 export const PATH_TO_MAIL_ICON = '/assets/newsletter/icon/mail.svg';
 export const PATH_TO_SIDEBAR_BUTTON_OPEN = '/assets/sidebar/open.svg';
 export const PATH_TO_SIDEBAR_BUTTON_CLOSE = '/assets/sidebar/close.svg';
-
-// export const PATH_TO_LOGO = '/public/assets/Circle.svg';
-// export const PATH_TO_HOME = '/index.html';
-// export const PATH_TO_PROJECTS = '/public/src/app/pages/projects/1.html';
-// export const PATH_TO_SERVISES = '/public/src/app/pages/servises/1.html';
-// export const PATH_TO_CONTACT = '/public/src/app/pages/contact/contact.html';
-// export const PATH_TO_TOGGLE = '/public/assets/sidebar/close.svg';
-// export const PATH_TO_MAIL_ICON = '/public/assets/newsletter/icon/mail.svg';
-// export const PATH_TO_SIDEBAR_BUTTON_OPEN = '/public/assets/sidebar/open.svg';
-// export const PATH_TO_SIDEBAR_BUTTON_CLOSE = '/public/assets/sidebar/close.svg';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        notFound: resolve(__dirname, '404.html'),
+      },
+    },
   },
+  publicDir: 'public',
 });
